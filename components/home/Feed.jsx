ca72766431b06cf59449e4d35e75c6d37fd1d3f2
FuryACE <sudhanshuvshekhar@gmail.com>
2022-11-22 02:12:57 +0530

    added post component

--- components/home/Feed.jsx
+++ components/home/Feed.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import PostBox from "./PostBox";
 import Post from "../Post";
 
+import avUrl from "/public/profilePic.jpg";
+
 const style = {
   wrapper: `flex-[2] border-r border-l border-[#38444d]`,
   header: `sticky top-0 bg-[#14171A] z-10 p-4 flex justify-between items-center`,
@@ -14,7 +16,15 @@ const posts = [
   {
     displayName: "SkaleDev",
     userName: "0x616225F50fA2b77F5e8e592468fa1cE37ba46a3a",
-    avatar: "/public/profilePic.jpg",
+    avatar: { avUrl },
+    text: "gm",
+    isProfileImageNft: true,
+    timestamp: "2020-06-01T12:00:00.000Z",
+  },
+  {
+    displayName: "SkaleDev",
+    userName: "0x616225F50fA2b77F5e8e592468fa1cE37ba46a3a",
+    avatar: { avUrl },
     text: "gm",
     isProfileImageNft: false,
     timestamp: "2020-06-01T12:00:00.000Z",
@@ -22,7 +32,7 @@ const posts = [
   {
     displayName: "SkaleDev",
     userName: "0x616225F50fA2b77F5e8e592468fa1cE37ba46a3a",
-    avatar: "/public/profilePic.jpg",
+    avatar: { avUrl },
     text: "gm",
     isProfileImageNft: false,
     timestamp: "2020-06-01T12:00:00.000Z",
@@ -30,7 +40,7 @@ const posts = [
   {
     displayName: "SkaleDev",
     userName: "0x616225F50fA2b77F5e8e592468fa1cE37ba46a3a",
-    avatar: "/public/profilePic.jpg",
+    avatar: { avUrl },
     text: "gm",
     isProfileImageNft: false,
     timestamp: "2020-06-01T12:00:00.000Z",
@@ -38,7 +48,7 @@ const posts = [
   {
     displayName: "SkaleDev",
     userName: "0x616225F50fA2b77F5e8e592468fa1cE37ba46a3a",
-    avatar: "/public/profilePic.jpg",
+    avatar: { avUrl },
     text: "gm",
     isProfileImageNft: false,
     timestamp: "2020-06-01T12:00:00.000Z",
@@ -46,15 +56,7 @@ const posts = [
   {
     displayName: "SkaleDev",
     userName: "0x616225F50fA2b77F5e8e592468fa1cE37ba46a3a",
-    avatar: "/public/profilePic.jpg",
-    text: "gm",
-    isProfileImageNft: false,
-    timestamp: "2020-06-01T12:00:00.000Z",
-  },
-  {
-    displayName: "SkaleDev",
-    userName: "0x616225F50fA2b77F5e8e592468fa1cE37ba46a3a",
-    avatar: "/public/profilePic.jpg",
+    avatar: { avUrl },
     text: "gm",
     isProfileImageNft: false,
     timestamp: "2020-06-01T12:00:00.000Z",
@@ -73,10 +75,11 @@ const Feed = () => {
         <Post
           key={index}
           displayName={post.displayName}
-          userName={post.userName}
+          userName={`${post.userName.slice(0, 4)}...${post.userName - 4}`}
           avatar={post.avatar}
           text={post.text}
           isProfileImageNft={post.isProfileImageNft}
+          timestamp={post.timestamp}
         />
       ))}
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Feed from "../components/home/Feed";
 import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
 
 const style = {
-  wrapper: `flex justify-center h-screen w-screen select-none bg-[#14171A] text-white`,
+  wrapper: `flex justify-center min-h-screen min-w-screen select-none bg-[#14171A] text-white`,
   content: `max-w-[1400px] w-2/3 flex justify-between`,
 };
 
@@ -16,7 +17,7 @@ export default function Home() {
       <div className={style.content}>
         <Sidebar />
         <Feed />
-        <h3>Widgets</h3>
+        <Widgets />
       </div>
     </div>
   );
