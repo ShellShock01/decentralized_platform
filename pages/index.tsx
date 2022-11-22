6359223bfb4134b528d0f598c470a28641aeb498
FuryACE <sudhanshuvshekhar@gmail.com>
2022-11-22 12:59:10 +0530

    added styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Widgets from "../components/Widgets";
 
 const style = {
   wrapper: `flex justify-center min-h-screen min-w-screen select-none bg-[#14171A] text-white`,
-  content: `max-w-[1400px] w-2/3 flex justify-between`,
+  content: `max-w-[1400px] w-4/5 flex justify-between`,
 };
 
 export default function Home() {
